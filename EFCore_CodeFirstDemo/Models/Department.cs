8bd329a70ba3e2f2b1a5c7b39600ceb1046b51d4
Subhash Kumar <Subhash Kumar@SWEETY>
2023-02-17 11:12:24 +0530

    Add project files.

---
+++ EFCore_CodeFirstDemo/Models/Department.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCore_CodeFirstDemo.Models
+{
+    public class Department
+    {
+        //public int Id {get; set; }
+        public int DepartmentId { get; set; }
+        //public int DeptId{get; set; }
+        public string DepartmentName { get; set; }
+        public string Location { get; set; }
+    }
+}
